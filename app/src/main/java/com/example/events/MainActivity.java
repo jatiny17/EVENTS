ae9c91cd796e9c147f78e379ca42b8aeb2a85eb1
Jatin Kumar <kumarjatinjt.1998@gmail.com>
2019-10-22 00:43:52 +0530

    Validation added

--- app/src/main/java/com/example/events/MainActivity.java
+++ app/src/main/java/com/example/events/MainActivity.java
@@ -5,11 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.basgeekball.awesomevalidation.ValidationStyle;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
@@ -18,6 +21,9 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     DatabaseReference data= FirebaseDatabase.getInstance().getReference();
+    Event e;
+
+    private AwesomeValidation awesomeValidation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,76 +42,51 @@ public class MainActivity extends AppCompatActivity {
         e4=(EditText)findViewById(R.id.event_location);
         e5=(EditText)findViewById(R.id.event_description);
 
+        awesomeValidation = new AwesomeValidation(ValidationStyle.BASIC);
+
+        awesomeValidation.addValidation(MainActivity.this, R.id.event_name, "^[A-Za-z\\s]{1,}[\\.]{0,1}[A-Za-z\\s]{0,}$", R.string.nameerror);
+        awesomeValidation.addValidation(MainActivity.this, R.id.event_date, "^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\\d\\d$", R.string.dateerror);
+//        awesomeValidation.addValidation(MainActivity.this, R.id.event_time, "\\b((1[0-2]|0?[0-9]):([0-5][0-9]) ([AaPp][Mm]))", R.string.timeerror);
+
+//      awesomeValidation.addValidation(MainActivity.this,R.id.event_location,,R.string.locationerror);
+//      awesomeValidation.addValidation(MainActivity.this,R.id.event_description,,R.string.descerror);
+
+
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 String event_name = null,event_time=null,event_date=null,event_location=null,event_desc=null;
 
-                try{
-                    event_name=e1.getText().toString();
-                }
+                event_name=e1.getText().toString();
+                event_date=e2.getText().toString();
+                event_time=e3.getText().toString();
+                event_location=e4.getText().toString();
+                event_desc=e5.getText().toString();
 
-                catch (Exception e)
-                {
-                    Toast.makeText(MainActivity.this, "Please enter name", Toast.LENGTH_SHORT).show();
-                }
+                if(!TextUtils.isEmpty(event_name)&&!TextUtils.isEmpty(event_date)&&!TextUtils.isEmpty(event_time)&&!TextUtils.isEmpty(event_location)&&!TextUtils.isEmpty(event_desc)) {
 
-                try {
-                    event_date=e2.getText().toString();
-                }
+                    if (awesomeValidation.validate())
+                    {
 
-                catch(Exception e)
-                {
-                    Toast.makeText(MainActivity.this, "Please enter date", Toast.LENGTH_SHORT).show();
-
-                }
-
-                try {
-                    event_time=e3.getText().toString();
-                }
-
-                catch(Exception e)
-                {
-                    Toast.makeText(MainActivity.this, "Please enter time", Toast.LENGTH_SHORT).show();
-
-                }
-
-                try {
-                    event_location=e4.getText().toString();
-                }
-
-                catch(Exception e)
-                {
-                    Toast.makeText(MainActivity.this, "Please enter location", Toast.LENGTH_SHORT).show();
-
-                }
-
-                try {
-                    event_desc=e5.getText().toString();
-                }
-
-                catch(Exception e)
-                {
-                    Toast.makeText(MainActivity.this, "Please enter description", Toast.LENGTH_SHORT).show();
-
-                }
-
-                Event e=new Event(event_name,event_date,event_time,event_location,event_desc);
-                data.child("events").push().setValue(e).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if(task.isSuccessful())
-                        {
-                            Toast.makeText(MainActivity.this, "Event Added Successfully", Toast.LENGTH_SHORT).show();
-                        }
-
-                        else
-                        {
-                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                        }
+                        e = new Event(event_name, event_date, event_time, event_location, event_desc);
+                        data.child("events").push().setValue(e).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(MainActivity.this, "Event Added Successfully", Toast.LENGTH_SHORT).show();
+                                } else {
+                                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                     }
-                });
+                }
+
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Fields should not be empty", Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
